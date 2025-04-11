06dfc9bd9e9aa035792c291127ec9c9c434b7b73
tkerr1159 <tkerr1@umd.edu>
2025-04-11 16:49:16 -0400

    fixing build

---
+++ src/app/_error.tsx
@@ -0,0 +1,23 @@
+// pages/_error.tsx
+import { NextPageContext } from 'next';
+
+type ErrorProps = {
+  statusCode?: number;
+};
+
+function Error({ statusCode }: ErrorProps) {
+  return (
+    <p>
+      {statusCode
+        ? `An error ${statusCode} occurred on server.`
+        : 'An error occurred on client.'}
+    </p>
+  );
+}
+
+Error.getInitialProps = ({ res, err }: NextPageContext) => {
+  const statusCode = res?.statusCode ?? err?.statusCode ?? 404;
+  return { statusCode };
+};
+
+export default Error;
